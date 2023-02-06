b03374d9003c66133b642e62044577577853039a
matsuzak8989 <123384972+matsuzak8989@users.noreply.github.com>
2023-02-06 22:29:32 +0900

    Create fade_up.js

---
+++ js/fade_up.js
@@ -0,0 +1,27 @@
+// $(window).on('load scroll',function(){
+//   $(".inview_re").on("inview_re", function (event, isInView) {
+//     if (isInView) {
+//       $(this).stop().addClass("is-show");
+//     } else {
+//       $(this).stop().removeClass("is-show");
+//     }
+//   });
+// });
+
+// $(window).on('scroll', function (){
+
+//   var inview = $('.fadeIn_up');
+//   var animated = 'animated';
+
+//   inview.each(function(){
+
+//   var inviewOffset = $(this).offset().top;
+//   var scrollPos = $(window).scrollTop();
+//   var wh = $(window).height();
+
+//   //画面内のどの位置で処理を実行するかで「100」の値を変更
+//   if(scrollPos > inviewOffset - wh + 10 ){
+//       $(this).toggleClass(animated);
+//   }
+//   });
+//   });
